12ba1c4cf72db57e4e0594b808ad4ab11a63fe20
Ales911 <ales911.nikol@gmail.com>
2023-06-14 16:57:42 +0300

    minor changes

--- hazelcast-client/readme.md
+++ hazelcast-client/readme.md
@@ -1,3 +1,16 @@
 mvn clean install 
 
 start java -jar target\hazelcast-client-0.0.1-SNAPSHOT.jar
+
+
+curl localhost:8081/book
+curl localhost:8081/book/xxx
+
+curl --location --request POST "localhost:8081/book" --header "Content-Type: application/json" --data-raw "{\"isbn\": \"isbn1\", \"author\": \"author1\", \"title\": \"title1\"}"
+curl --location --request POST "localhost:8081/book" --header "Content-Type: application/json" --data-raw "{\"isbn\": \"isbn2\", \"author\": \"author2\", \"title\": \"title2\"}"
+curl --location --request POST "localhost:8081/book" --header "Content-Type: application/json" --data-raw "{\"isbn\": \"isbn3\", \"author\": \"author3\", \"title\": \"title3\"}"
+curl --location --request POST "localhost:8081/book" --header "Content-Type: application/json" --data-raw "{\"isbn\": \"isbn4\", \"author\": \"author4\", \"title\": \"title4\"}"
+
+curl --location --request PUT "localhost:8081/book/xxx" --header "Content-Type: application/json" --data-raw "{\"isbn\": \"isbn3\", \"author\": \"author33\", \"title\": \"title33\"}"
+
+curl --location --request DELETE "localhost:8081/book/xxx"
--- hazelcast-client/src/main/java/com/example/BookService.java
+++ hazelcast-client/src/main/java/com/example/BookService.java
@@ -10,35 +10,45 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     private static final Logger logger = LoggerFactory.getLogger(BookService.class);
-    
+
     @Autowired
     private HazelcastService hazelcastService;
-    
+
     public Collection<Book> findAll() {
         logger.info("findAll(): ");
         return hazelcastService.getMapDev().values();
     }
-    
+
     public Book add(Book book) {
         logger.info("add a book with isbn: " + book.getIsbn());
         Long id = hazelcastService.getNewId();
         book.setId(id);
         return hazelcastService.getMapDev().putIfAbsent(id, book);
     }
-    
+
     public Book find(Long id) {
         logger.info("finding book by id: " + id);
+        checkId(id);
         return hazelcastService.getMapDev().get(id);
     }
-    
-    public Book update(Long id, Book book) {
+
+    public Book update(final Long id, final Book book) {
         logger.info("update book with isbn: " + book.getIsbn());
+        checkId(id);
+        book.setId(id);
         return hazelcastService.getMapDev().replace(id, book);
     }
-    
-    public Book remove(Long id) {
+
+    public Book remove(final Long id) {
         logger.info("remove book by id: " + id);
+        checkId(id);
         return hazelcastService.getMapDev().remove(id);
+    }
+
+    private void checkId(final Long id) {
+        if ((id == null) || !hazelcastService.getMapDev().containsKey(id)) {
+            throw new IllegalArgumentException("No book found with id: " + id);
+        }
     }
 
 }
